170c18b1ba2911c261da83c0ceed2bcb3e51b1eb
re-21-12 <victora.caas@gmail.com>
2025-09-15 17:56:25 -0600

    [FIX] fixed auth s

--- tablero_api/Models/DTOS/AuthDto.cs
+++ tablero_api/Models/DTOS/AuthDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace tablero_api.Models.DTOS
 {
 public record LoginRequestDto(string Nombre, string Contrasena);
-public record LoginResponseDto(string? Token, string? Nombre, RolDto? Rol, string RefresToken);
+public record LoginResponseDto(string? Token, string? Nombre, RolDto? Rol, string RefreshToken);
 public record RolDto(string Nombre);
 public record RegisterRequestDto(string Nombre, string Contrasena, RolDto Rol);
     public record RefreshRequestDto(string RefreshToken);
